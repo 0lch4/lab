c7adf03d22eb99e4cf02919c4361ce7a5809c500
0lch4 <127250399+0lch4@users.noreply.github.com>
2023-04-02 20:55:41 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -5,8 +5,8 @@ if [ "$1" = "--logs" ]; then
 for i in {1..100}
 do
     touch log$i.txt
-    echo "Nazwa pliku: log$i.txt" >> log$i.txt
-    echo "Nazwa skryptu: $0" >> log$i.txt
+    echo "Nazwa Pliku: log$i.txt" >> log$i.txt
+    echo "Nazwa Skryptu: $0" >> log$i.txt
     echo "Data: $(date)" >> log$i.txt
 done
 
