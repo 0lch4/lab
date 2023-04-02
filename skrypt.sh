c8c3f9685897eabbcc43f9b10e2df49f193ada6b
0lch4 <127250399+0lch4@users.noreply.github.com>
2023-04-02 20:28:39 +0200

    Create skrypt.sh

---
+++ skrypt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$1" = "--logs" ]; then
+
+for i in {1..100}
+do
+    touch log$i.txt
+    echo "Nazwa pliku: log$i.txt" >> log$i.txt
+    echo "Nazwa skryptu: $0" >> log$i.txt
+    echo "Data: $(date)" >> log$i.txt
+done
+
+fi
