d874aab5caf2ce9590de2593ac9717d4b3540ed4
0lch4 <127250399+0lch4@users.noreply.github.com>
2023-04-03 21:33:52 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -34,6 +34,21 @@ do
     echo "Data: $(date)" >> error$i/error$i.txt
 done
 
+if [ "$1" = "--error" ] && [ "$2" = "-e" ] && [ "$3" -gt 0 ] 2>/dev/null;then
+
+for ((i=1; i<=$3; i++))
+
+
+do
+    mkdir error$i
+    touch error$i/error$i.txt
+    echo "Nazwa Pliku: error$i.txt" >> error$i/error$i.txt
+    echo "Nazwa Skryptu: $0" >> error$i/error$i.txt
+    echo "Data: $(date)" >> error$i/error$i.txt
+done
+
+fi
+
 if [ "$1" = "--logs" ]; then
 
 for ((i=1; i<=100; i++))
