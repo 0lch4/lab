1d95b8ddc306e14940468696446f75dd0ddbb248
0lch4 <127250399+0lch4@users.noreply.github.com>
2023-04-03 21:33:29 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -58,6 +58,18 @@ if [ "$1" = "--logs" ] && [ "$2" -gt 0 ] 2>/dev/null; then
     done
 fi
 
+
+if [ "$1" = "--logs" ] && [ "$2" = "-l" ] && [ "$3" -gt 0 ] 2>/dev/null; then
+    for ((i=1; i<=$3; i++))
+    do
+        touch log$i.txt
+        echo "Nazwa pliku: log$i.txt" >> log$i.txt
+        echo "Nazwa skryptu: $0" >> log$i.txt
+        echo "Data utworzenia: $(date)" >> log$i.txt
+    done
+fi
+
+
 if [ "$1" = "--help" ] && [ "$2" = "-h" ]; then
 
 echo "skrypt.sh --date -d - wyswietli dzisiejsza date"
