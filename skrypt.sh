5741aeca852c3b750562f191a6bc36d7b074f5af
0lch4 <127250399+0lch4@users.noreply.github.com>
2023-04-03 17:42:14 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -5,4 +5,7 @@ if [ "$1" = "--help" ]; then
 echo "skrypt.sh --date - wyswietli dzisiejsza date"
 echo "skrypt.sh --logs - utworzy 100 plikow logx.txt gdzie x to numer pliku"
 echo "skrypt.sh --logs30 - utworzy 30 plikow jak wyzej(mozna podac zamiast 30 dowolna liczbe"
+echo "skrypt.sh --help -h"
+echo "skrypt.sh --logs -l"
+echo "skrypt.sh --date -d"
 fi
